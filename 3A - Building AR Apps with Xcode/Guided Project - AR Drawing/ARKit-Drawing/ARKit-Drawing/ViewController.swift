f8c53323b256be3560bfd9fcc5a26de0cf00b419
tali-bd <taliblando@gmail.com>
2025-05-09 15:35:29 -0700

    Working on AR Drawing program

--- 3A - Building AR Apps with Xcode/Guided Project - AR Drawing/ARKit-Drawing/ARKit-Drawing/ViewController.swift
+++ 3A - Building AR Apps with Xcode/Guided Project - AR Drawing/ARKit-Drawing/ARKit-Drawing/ViewController.swift
@@ -4,6 +4,8 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    var selectedNode: SCNNode?
+    
     @IBOutlet var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
     
@@ -29,6 +31,22 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         super.viewWillDisappear(animated)
         sceneView.session.pause()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        //Check if selected node has value
+        guard let node = selectedNode, let touch = touches.first else { return }
+        
+        switch objectMode {
+        case .freeform:
+            addNodeInFront(node)
+        case .plane:
+            break
+        case .image:
+            break
+        }
+    }
 
     @IBAction func changeObjectMode(_ sender: UISegmentedControl) {
         switch sender.selectedSegmentIndex {
@@ -49,12 +67,41 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             optionsViewController.delegate = self
         }
     }
+    
+    func addNodeInFront(_ node: SCNNode) {
+        guard let currentFrame = sceneView.session.currentFrame else { return }
+        
+        // Set transform of node to be 20cm in front of camera
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -0.2
+        node.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        
+        let cloneNode = node.clone()
+        sceneView.scene.rootNode.addChildNode(cloneNode)
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        if let imageAnchor = anchor as? ARImageAnchor {
+            nodeAdded(node, for: imageAnchor)
+        } else if let planeAnchor = anchor as? ARPlaneAnchor {
+            nodeAdded(node, for: planeAnchor)
+        }
+    }
+
+    func nodeAdded(_ node: SCNNode, for anchor: ARPlaneAnchor) {
+
+    }
+
+    func nodeAdded(_ node: SCNNode, for anchor: ARImageAnchor) {
+
+    }
 }
 
 extension ViewController: OptionsViewControllerDelegate {
     
     func objectSelected(node: SCNNode) {
         dismiss(animated: true, completion: nil)
+        selectedNode = node
     }
     
     func togglePlaneVisualization() {
