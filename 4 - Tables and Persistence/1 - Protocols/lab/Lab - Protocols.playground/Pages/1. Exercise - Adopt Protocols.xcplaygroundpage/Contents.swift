9d6fd46beda29060bc228457515552a8c10f4833
tali-bd <taliblando@gmail.com>
2025-05-16 16:21:59 -0700

    Protocols
    Working on protocols lab; left off on section 2

--- 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
+++ 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/1. Exercise - Adopt Protocols.xcplaygroundpage/Contents.swift
@@ -3,21 +3,60 @@
  
  Create a `Human` class with two properties: `name` of type `String`, and `age` of type `Int`. You'll need to create a memberwise initializer for the class. Initialize two `Human` instances.
  */
+class Human: CustomStringConvertible, Equatable, Comparable, Codable{
+    var name: String
+    var age: Int
+    
+    init(name: String, age: Int)
+    {
+        self.name = name
+        self.age = age
+    }
+    
+    var description: String{
+        return("Human(Name: \(name), Age: \(age))")
+    }
+    
+    static func ==(lhs: Human, rhs: Human) -> Bool
+    {
+            return (lhs.name == rhs.name) && (lhs.age == rhs.age)
+    }
+    
+    static func <(lhs: Human, rhs: Human) -> Bool
+    {
+        return(lhs.age < rhs.age)
+    }
+}
 
+var human1 = Human(name: "Mandy", age: 24)
+var human2 = Human(name: "Missy", age: 52)
 
 /*:
  Make the `Human` class adopt the `CustomStringConvertible`. Print both of your previously initialized `Human` objects.
  */
-
+print("Human1: \(human1), Human2: \(human2)")
 
 /*:
  Make the `Human` class adopt the `Equatable` protocol. Two instances of `Human` should be considered equal if their names and ages are identical to one another. Print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are equal to eachother (using `==`). Then print the result of a boolean expression evaluating whether or not your two previously initialized `Human` objects are not equal to eachother (using `!=`).
  */
-
+print("Are human1 and human2 equal to each other? Results say: \(human1==human2)")
+print("Are human1 and human2 different from each other? Results say: \(human1 != human2)")
 
 /*:
  Make the `Human` class adopt the `Comparable` protocol. Sorting should be based on age. Create another three instances of a `Human`, then create an array called `people` of type `[Human]` with all of the `Human` objects that you have initialized. Create a new array called `sortedPeople` of type `[Human]` that is the `people` array sorted by age.
  */
+var human3 = Human(name: "Mike", age: 34)
+var human4 = Human(name: "Mary", age: 14)
+var human5 = Human(name: "Marvin", age: 89)
+
+var people: [Human] = [human1, human2, human3, human4, human5]
+var sortedPeople = people.sorted(by: <)
+
+
+for person in sortedPeople{
+    print(person)
+}
+
 
 
 /*:
@@ -25,5 +64,9 @@
  */
 import Foundation
 
-
+let json = JSONEncoder()
+if let jsonData = try? json.encode(human1),
+    let jsonString = String(data: jsonData, encoding: .utf8) {
+    print(jsonString)
+}
 //: page 1 of 5  |  [Next: App Exercise - Printable Workouts](@next)
--- 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/2. App Exercise - Printable Workouts.xcplaygroundpage/Contents.swift
+++ 4 - Tables and Persistence/1 - Protocols/lab/Lab - Protocols.playground/Pages/2. App Exercise - Printable Workouts.xcplaygroundpage/Contents.swift
@@ -5,7 +5,7 @@
  
  The `Workout` objects you have created so far in app exercises don't show a whole lot of useful information when printed to the console. They also aren't very easy to compare or sort. Throughout these exercises, you'll make the `Workout` class below adopt certain protocols that will solve these issues.
  */
-class Workout {
+class Workout: CustomStringConvertible, Equatable {
     var distance: Double
     var time: Double
     var identifier: Int
@@ -15,24 +15,51 @@ class Workout {
         self.time = time
         self.identifier = identifier
     }
+    
+    var description: String{
+        return("Your current distance is: \(distance) \nYour current time is: \(time) \nYour identifier is: \(identifier)")
+    }
+    
+    static func ==(lhs:Workout, rhs:Workout) -> Bool
+    {
+        return(lhs.identifier == rhs.identifier)
+    }
+    
+    static func< (lhs:Workout, rhs:Workout) -> Bool
+    {
+        return (lhs.time < rhs.time)
+    }
 }
+
 
 /*:
  Make the `Workout` class above conform to the `CustomStringConvertible` protocol so that printing an instance of `Workout` will provide useful information in the console. Create an instance of `Workout`, give it an identifier of 1, and print it to the console.
  */
-
+var workout1 = Workout(distance: 0, time: 0, identifier: 1)
+print(workout1)
 
 /*:
  Make the `Workout` class above conform to the `Equatable` protocol. Two `Workout` objects should be considered equal if they have the same identifier. Create another instance of `Workout`, giving it an identifier of 2, and print a boolean expression that evaluates to whether or not it is equal to the first `Workout` instance you created.
  */
+var workout2 = Workout(distance: 0, time: 0, identifier: 2)
 
+print("Are the two workouts the same? Results say: \(workout1 == workout2)")
 
 /*:
  Make the `Workout` class above conform to the `Comparable` protocol so that you can easily sort multiple instances of `Workout`. `Workout` objects should be sorted based on their identifier. 
  
  Create three more `Workout` objects, giving them identifiers of 3, 4, and 5, respectively. Then create an array called `workouts` of type `[Workout]` and assign it an array literal with all five `Workout` objects you have created. Place these objects in the array out of order. Then create another array called `sortedWorkouts` of type `[Workout]` that is the `workouts` array sorted by identifier. 
  */
+var workout3 = Workout(distance: 1.0, time: 2.0, identifier: 3)
+var workout4 = Workout(distance: 2.3, time: 4.2, identifier: 4)
+var workout5 = Workout(distance: 1.5, time: 2.3, identifier: 5)
 
+var workouts : [Workout] = [workout1, workout2, workout3, workout4, workout5]
+var sortedWorkouts = workouts.sorted(by: <)
+
+for work in sortedWorkouts{
+    print(work)
+}
 
 /*:
  Make `Workout` adopt the `Codable` protocol so you can easily encode `Workout` objects as data that can be stored between app launches. Use a `JSONEncoder` to encode one of your `Workout` instances. Then use the encoded data to initialize a `String`, and print it to the console.
